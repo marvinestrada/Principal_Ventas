0d373d235f33a066c032e35898a61f850984ae1b
Fonseca1998 <62191695+Fonseca1998@users.noreply.github.com>
2020-05-20 22:32:15 -0600

    Cmbios New

--- Ventas/Vistas/AddPermiso_Personas.xaml.cs
+++ Ventas/Vistas/AddPermiso_Personas.xaml.cs
@@ -105,6 +105,26 @@ namespace ProyectoTienda.Vistas
             //Conexcion con la BD mostrando los datos y modicando los ya ingresados
         }
 
+        private void txtpermiso_KeyDown(object sender, KeyEventArgs e)
+        {
+            if ((e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9) || (e.Key == Key.Back))
+            { e.Handled = false; }
+            else if ((e.Key >= Key.D0 && e.Key <= Key.D9))
+            { e.Handled = false; }
+            else
+            { e.Handled = true; }
+        }
+
+        private void txtempleado_KeyDown(object sender, KeyEventArgs e)
+        {
+            if ((e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9) || (e.Key == Key.Back))
+            { e.Handled = false; }
+            else if ((e.Key >= Key.D0 && e.Key <= Key.D9))
+            { e.Handled = false; }
+            else
+            { e.Handled = true; }
+        }
+
         private void button_Click_3(object sender, RoutedEventArgs e)
         {
 

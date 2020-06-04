7eab82a6c3e639e876d9c1f82fbe8fb84d77a378
Angela Castellanos <angela-castellanos@hotmail.com>
2020-06-03 23:47:14 -0600

    Reportería/stock, compras y ventas

---
+++ Ventas/Vistas/ReportStock.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProyectoTienda
+{
+    /// <summary>
+    /// Lógica de interacción para Pagos.xaml
+    /// </summary>
+    public partial class ReportStock : Window
+    {
+        public ReportStock()
+        {
+            InitializeComponent();
+        }
+
+        private void mover(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void btnCerrar(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Ventana_reporte (object sender, RoutedEventArgs e)
+        {
+            ReporteStock abrir = new ReporteStock();
+            abrir.Load(@"ReporteStock.rep");
+            VerDatos.ViewerCore.ReportSource = abrir;
+
+        }
+
+    }
+}
---
+++ Ventas/Vistas/ReportVentas.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProyectoTienda
+{
+    /// <summary>
+    /// Lógica de interacción para ReportVentas.xaml
+    /// </summary>
+    public partial class ReportVentas : Window
+    {
+        public ReportVentas()
+        {
+            InitializeComponent();
+        }
+
+        private void mover(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void btnCerrar(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Vent_reporte(object sender, RoutedEventArgs e)
+        {
+            ReporteVentas abrir = new ReporteVentas();
+            abrir.Load(@"ReporteVentas.rep");
+            VerDatos.ViewerCore.ReportSource = abrir;
+
+        }
+    }
+}
